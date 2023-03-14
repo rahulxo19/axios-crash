de5399f3642010de39f191588a167d544edcf415
rahulxo19 <rahulchhikara2001@gmail.com>
2023-03-14 08:17:42 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const fs = require("fs");
+const p = path.join(__dirname, "..", "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          // Handle the write error
+          console.log(err);
+        }
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
