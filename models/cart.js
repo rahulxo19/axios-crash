79fc4d24696395fefeedd30752937becce905f70
rahulxo19 <rahulchhikara2001@gmail.com>
2023-03-16 14:27:19 +0530

    query Params

--- models/cart.js
+++ models/cart.js
@@ -10,29 +10,52 @@ const p = path.join(
 module.exports = class Cart {
     static addProduct(id, productPrice) {
         fs.readFile(p, (err, fileContent) => {
-            let cart = {products: [], totalPrice: 0};
-            if(!err) {
-                cart = JSON.parse(fileContent);
-            }
-            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
-            const existingProduct = cart.products[existingProductIndex];
-            let updatedProduct;
-
-            if(existingProduct){
-                updatedProduct = {...existingProduct};
+            if (err) {
+              console.log(err);
+            } else {
+              let cart = { products: [], totalPrice: 0 };
+              if (fileContent) {
+                try {
+                  cart = JSON.parse(fileContent);
+                } catch (error) {
+                  console.log(error.message);
+                }
+              }
+              const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+              const existingProduct = cart.products[existingProductIndex];
+              let updatedProduct;
+          
+              if (existingProduct) {
+                updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
                 cart.products = [...cart.products];
                 cart.products[existingProductIndex] = updatedProduct;
-            }else {
+              } else {
                 updatedProduct = { id: id, qty: 1 };
                 cart.products = [...cart.products, updatedProduct];
+              }
+              cart.totalPrice = cart.totalPrice + +productPrice;
+              fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+              });
             }
-            cart.totalPrice = cart.totalPrice + +productPrice;
-            fs.writeFile(p, JSON.stringify(cart), (err) => {
+          });
+    }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if(err) {
+                return;
+            }
+            const cart = JSON.parse(fileContent);
+            const updatedCart = {...cart };
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice*productQty;
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
                 console.log(err);
             })
         })
     }
-
-
 }
--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -24,7 +25,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -32,13 +34,22 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+      if(this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+      })}else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+
+      }
+    })
   }
 
   static fetchAll(cb) {
@@ -49,6 +60,20 @@ module.exports = class Product {
     getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
+    })
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(p => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          Cart.deleteProduct(id, product.price);
+        }else {
+          console.log(err);
+        }
+      })
     })
   }
 };
