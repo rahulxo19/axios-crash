c08609c8c744467a7520beadc0ca8867663eafd9
rahulxo19 <rahulchhikara2001@gmail.com>
2023-03-19 12:22:04 +0530

    mySql learned

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,10 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing: false
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -13,55 +13,70 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, price, imageUrl, description);
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if(!editMode){
-    res.redirect('/');
+  if (!editMode) {
+    res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if(!product){
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-
-  })
+  Product.findById(prodId)
+    .then(([rows]) => {
+      console.log(rows[0]);
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: rows[0],
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
-const prodId = req.body.productId;
-const updatedTitle = req.body.title;
-const updatedPrice = req.body.price;
-const updatedImageUrl = req.body.imageUrl;
-const updatedDesc = req.body.description;
-const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDesc)
-updatedProduct.save();
-res.redirect('/admin/products')
-}
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = Number(req.body.price);
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedImageUrl,
+    updatedDesc
+  );
+  console.log(updatedPrice);
+  updatedProduct
+    .update(prodId)
+    .then(() => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows]) => {
+      res.render("admin/products", {
+        prods: rows,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.delProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
-  Product.deleteById(prodId);
-  res.redirect('/admin/products')
-}
+  Product.deleteById(prodId).then(() => {
+    res.redirect("/admin/products");
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,79 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../util/database')
 const Cart = require('./cart')
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      try{
-        cb(JSON.parse(fileContent));
-      } catch (error) {
-        if(error.message === "Unexpected end of JSON input"){
-          cb([]);
-        }
-      }
-    }
-  });
-};
+
 
 module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
+  constructor(title,price, imageUrl, description ) {
     this.title = title;
+    this.price = Number(price);
     this.imageUrl = imageUrl;
     this.description = description;
-    this.price = price;
   }
 
   save() {
-    getProductsFromFile(products => {
-      if(this.id) {
-        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-      })}else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-
-      }
-    })
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+    [this.title, this.price, this.imageUrl, this.description]
+    )
+  }
+  update(id) {
+    return db.execute('UPDATE products SET title = ?, price = ?, imageUrl = ?, description = ? WHERE id=?',
+    [this.title, this.price, this.imageUrl, this.description, id])
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    })
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(p => p.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if(!err) {
-          Cart.deleteProduct(id, product.price);
-        }else {
-          console.log(err);
-        }
-      })
-    })
+    return db.execute('DELETE FROM products WHERE products.id = ?', [id])
   }
-};
+}
